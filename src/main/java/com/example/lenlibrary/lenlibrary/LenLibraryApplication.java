0992258779d1c64de0517186a5026736cf508686
KristiantoJohan <87749195+KristiantoJohan@users.noreply.github.com>
2024-01-05 15:33:33 +0700

    Add files via upload

---
+++ src/main/java/com/example/lenlibrary/lenlibrary/LenLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.lenlibrary.lenlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LenLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LenLibraryApplication.class, args);
+	}
+
+}
